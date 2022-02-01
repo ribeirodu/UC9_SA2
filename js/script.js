e8b826beef2e84fb8c067520e925842b6c27bcd1
ribeirodu <ribeiro.du@gmail.com>
2022-01-31 21:42:39 -0300

    Finalização - aula 31/01/2022

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// function mostrarPopup(){
+//     window.alert("Novas skins disponíveis para Tetris!")
+// }
+
+function enviarNewsletter(){
+    let email = document.getElementById("campo-email");
+    let emailDigitado = email.value;
+    console.log(emailDigitado)
+}
